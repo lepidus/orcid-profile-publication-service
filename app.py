4589713ea5248c797fa37a1ea56778780db52df8
YvesLepidus <yves@lepidus.com.br>
2025-03-17 19:25:27 -0400

    Moves create_table method to other scope.

--- app.py
+++ app.py
@@ -195,7 +195,8 @@ def process_authorization(request_id):
 def create_tables():
     db.create_all()
 
+with app.app_context():
+    create_tables()
+
 if __name__ == "__main__":
-    with app.app_context():
-        create_tables()
     app.run(host='0.0.0.0', port=5100)
