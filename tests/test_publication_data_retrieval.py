b41e5bca6812d693780f00d37185fc2b0ef03251
YvesLepidus <yves@lepidus.com.br>
2025-04-10 18:17:31 -0400

    Implements external ID retrieval.
    Issue: documentacao-e-tarefas/scielo#767
    
    Signed-off-by: YvesLepidus <yves@lepidus.com.br>

--- tests/test_publication_data_retrieval.py
+++ tests/test_publication_data_retrieval.py
@@ -8,7 +8,16 @@ class TestPublicationDataRetrieval(unittest.TestCase):
     def setUp(self):
         self.work_data = {
             "title": {'title': {'value': 'Exemplo de Publicação'}, 'subtitle': {'value': 'Um subtítulo interessante'}},
-            "journal-title": {'value': 'Revista de Exemplo'}
+            "journal-title": {'value': 'Revista de Exemplo'},
+            "external-ids": {
+                "external-id": [
+                    {
+                        "external-id-type": "doi",
+                        "external-id-value": "10.1000/xyz123",
+                        "external-id-url": {"value": "http://example.com"}
+                    }
+                ]
+            },
         }
         self.publication_data_retrieval = PublicationDataRetrieval(self.work_data)
     
@@ -21,6 +30,11 @@ class TestPublicationDataRetrieval(unittest.TestCase):
         expected_title = "Revista de Exemplo"
         journal_title = self.publication_data_retrieval.get_journal_title()
         self.assertEqual(expected_title, journal_title)
+    
+    def test_external_id_retrieval(self):
+        expected_external_id = "10.1000/xyz123"
+        external_id = self.publication_data_retrieval.get_external_id()
+        self.assertEqual(expected_external_id, external_id)
     
 if __name__ == "__main__":
     unittest.main()
